984e23b9c92ec216563d87a7b354788592969910
Md Nuruddin Osman <mdnuruddin378350@gmail.com>
2024-01-16 15:26:12 +0600

    Product dynamic 01

--- src/components/home/NewArrivals.jsx
+++ src/components/home/NewArrivals.jsx
@@ -10,6 +10,7 @@ import { FiRefreshCw } from "react-icons/fi";
 import { FaShoppingCart } from "react-icons/fa";
 import { Link } from 'react-router-dom';
 import SectionHeading from '../utilities/SectionHeading';
+import ProductCard from './ProductCard'
 
 const NewArrivals = () => {
   return (
@@ -17,138 +18,11 @@ const NewArrivals = () => {
         <div className="mb-[118px]">
             <SectionHeading/>
             <Flex className="gap-10 justify-between flex-wrap">
-              <div className="w-[370px] h-[465px] shadow-lg">
-                  <div className="h-[370px] overflow-hidden relative group">
-                    <Image className="h-full w-full object-cover" source={ArrivalsProduct1}/>
-                    <div className="absolute top-5 left-5">
-                      <span className='py-[10px] px-8 text-[#FFFFFF] text-sm font-bold font-dm bg-[#262626]'>New</span>
-                    </div>
-                    <div className="w-full h-[156px] bg-white absolute bottom-0 translate-y-full group-hover:translate-y-0 transition-all duration-500 p-6">
-                      <ul className='flex flex-col gap-5'>
-                        <li className="flex gap-4 items-center justify-end cursor-pointer">
-                          <Link className='text-base text-[#767676] font-dm hover:text-[#000000] transition duration-300'>Add to Wish List</Link>
-                          <FaHeart className='text-sm' />
-                        </li>
-                        <li className="flex gap-4 items-center justify-end cursor-pointer">
-                          <Link className='text-base text-[#767676] font-dm hover:text-[#000000] transition duration-300'>Compare</Link>
-                          <FiRefreshCw className='text-sm' />
-                        </li>
-                        <li className="flex gap-4 items-center justify-end cursor-pointer">
-                          <Link className='text-base text-[#767676] font-dm hover:text-[#000000] transition duration-300'>Add to Cart</Link>
-                          <FaShoppingCart className='text-sm' />
-                        </li>
-                      </ul>
-                    </div>
-                  </div>
-                  <div className="">
-                    <Flex className="justify-between items-center my-4">
-                    <h5 className='text-[#262626] font-bold font-dm text-xl'>Basic Crew Neck Tee</h5>
-                    <span className='text-[#767676] font-normal font-dm text-base leading-[30px]'>$44.00</span>
-                    </Flex>
-                    <div className="">
-                      <span className='text-[#767676] font-normal font-dm text-base leading-[30px]'>Black</span>
-                    </div>
-                  </div>
-              </div>
-              <div className="w-[370px] h-[465px] shadow-lg">
-                  <div className="h-[370px] overflow-hidden relative group">
-                    <Image className="h-full w-full object-cover" source={ArrivalsProduct2}/>
-                    <div className="absolute top-5 left-5">
-                      <span className='py-[10px] px-8 text-[#FFFFFF] text-sm font-bold font-dm bg-[#262626]'>New</span>
-                    </div>
-                    <div className="w-full h-[156px] bg-white absolute bottom-0 translate-y-full group-hover:translate-y-0 transition-all duration-500 p-6">
-                      <ul className='flex flex-col gap-5'>
-                        <li className="flex gap-4 items-center justify-end cursor-pointer">
-                          <Link className='text-base text-[#767676] font-dm hover:text-[#000000] transition duration-300'>Add to Wish List</Link>
-                          <FaHeart className='text-sm' />
-                        </li>
-                        <li className="flex gap-4 items-center justify-end cursor-pointer">
-                          <Link className='text-base text-[#767676] font-dm hover:text-[#000000] transition duration-300'>Compare</Link>
-                          <FiRefreshCw className='text-sm' />
-                        </li>
-                        <li className="flex gap-4 items-center justify-end cursor-pointer">
-                          <Link className='text-base text-[#767676] font-dm hover:text-[#000000] transition duration-300'>Add to Cart</Link>
-                          <FaShoppingCart className='text-sm' />
-                        </li>
-                      </ul>
-                    </div>
-                  </div>
-                  <div className="">
-                    <Flex className="justify-between items-center my-4">
-                    <h5 className='text-[#262626] font-bold font-dm text-xl'>Basic Crew Neck Tee</h5>
-                    <span className='text-[#767676] font-normal font-dm text-base leading-[30px]'>$44.00</span>
-                    </Flex>
-                    <div className="">
-                      <span className='text-[#767676] font-normal font-dm text-base leading-[30px]'>Black</span>
-                    </div>
-                  </div>
-              </div>
-              <div className="w-[370px] h-[465px] shadow-lg">
-                  <div className="h-[370px] overflow-hidden relative group">
-                    <Image className="h-full w-full object-cover" source={ArrivalsProduct3}/>
-                    <div className="absolute top-5 left-5">
-                      <span className='py-[10px] px-8 text-[#FFFFFF] text-sm font-bold font-dm bg-[#262626]'>New</span>
-                    </div>
-                    <div className="w-full h-[156px] bg-white absolute bottom-0 translate-y-full group-hover:translate-y-0 transition-all duration-500 p-6">
-                      <ul className='flex flex-col gap-5'>
-                        <li className="flex gap-4 items-center justify-end cursor-pointer">
-                          <Link className='text-base text-[#767676] font-dm hover:text-[#000000] transition duration-300'>Add to Wish List</Link>
-                          <FaHeart className='text-sm' />
-                        </li>
-                        <li className="flex gap-4 items-center justify-end cursor-pointer">
-                          <Link className='text-base text-[#767676] font-dm hover:text-[#000000] transition duration-300'>Compare</Link>
-                          <FiRefreshCw className='text-sm' />
-                        </li>
-                        <li className="flex gap-4 items-center justify-end cursor-pointer">
-                          <Link className='text-base text-[#767676] font-dm hover:text-[#000000] transition duration-300'>Add to Cart</Link>
-                          <FaShoppingCart className='text-sm' />
-                        </li>
-                      </ul>
-                    </div>
-                  </div>
-                  <div className="">
-                    <Flex className="justify-between items-center my-4">
-                    <h5 className='text-[#262626] font-bold font-dm text-xl'>Basic Crew Neck Tee</h5>
-                    <span className='text-[#767676] font-normal font-dm text-base leading-[30px]'>$44.00</span>
-                    </Flex>
-                    <div className="">
-                      <span className='text-[#767676] font-normal font-dm text-base leading-[30px]'>Black</span>
-                    </div>
-                  </div>
-              </div>
-              <div className="w-[370px] h-[465px] shadow-lg">
-                  <div className="h-[370px] overflow-hidden relative group">
-                    <Image className="h-full w-full object-cover" source={ArrivalsProduct4}/>
-                    <div className="absolute top-5 left-5">
-                      <span className='py-[10px] px-8 text-[#FFFFFF] text-sm font-bold font-dm bg-[#262626]'>New</span>
-                    </div>
-                    <div className="w-full h-[156px] bg-white absolute bottom-0 translate-y-full group-hover:translate-y-0 transition-all duration-500 p-6">
-                      <ul className='flex flex-col gap-5'>
-                        <li className="flex gap-4 items-center justify-end cursor-pointer">
-                          <Link className='text-base text-[#767676] font-dm hover:text-[#000000] transition duration-300'>Add to Wish List</Link>
-                          <FaHeart className='text-sm' />
-                        </li>
-                        <li className="flex gap-4 items-center justify-end cursor-pointer">
-                          <Link className='text-base text-[#767676] font-dm hover:text-[#000000] transition duration-300'>Compare</Link>
-                          <FiRefreshCw className='text-sm' />
-                        </li>
-                        <li className="flex gap-4 items-center justify-end cursor-pointer">
-                          <Link className='text-base text-[#767676] font-dm hover:text-[#000000] transition duration-300'>Add to Cart</Link>
-                          <FaShoppingCart className='text-sm' />
-                        </li>
-                      </ul>
-                    </div>
-                  </div>
-                  <div className="">
-                    <Flex className="justify-between items-center my-4">
-                    <h5 className='text-[#262626] font-bold font-dm text-xl'>Basic Crew Neck Tee</h5>
-                    <span className='text-[#767676] font-normal font-dm text-base leading-[30px]'>$44.00</span>
-                    </Flex>
-                    <div className="">
-                      <span className='text-[#767676] font-normal font-dm text-base leading-[30px]'>Black</span>
-                    </div>
-                  </div>
-              </div>
+              <ProductCard productName="Basic Crew Neck Tee" productImage={ArrivalsProduct1} productPrice="$44.00" isNew="true" discount="" color=""/>
+              <ProductCard productName="Basic Crew Neck Tee" productImage={ArrivalsProduct2} productPrice="$44.00" isNew="false" discount="" color=""/>
+              <ProductCard productName="Basic Crew Neck Tee" productImage={ArrivalsProduct3} productPrice="$44.00" isNew="true" discount="" color=""/>
+              <ProductCard productName="Basic Crew Neck Tee" productImage={ArrivalsProduct4} productPrice="$44.00" isNew="false" discount="" color=""/>
+              
             </Flex>
         </div>
     </section>
---
+++ src/components/home/ProductCard.jsx
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from 'react'
+import Flex from '../utilities/Flex'
+import { Link } from 'react-router-dom'
+import { FaHeart } from "react-icons/fa";
+import { FiRefreshCw } from "react-icons/fi";
+import { FaShoppingCart } from "react-icons/fa";
+import Image from '../utilities/Image';
+
+const ProductCard = ({productName,productImage,productPrice,isNew,discount}) => {
+  let [newProduct, setNewProduc] = useState(isNew)
+  useEffect(()=>{
+    if (isNew == "true") {
+      setNewProduc(true)
+    }else{
+      setNewProduc(false)
+    }
+  },[])
+  return (
+    <Flex className="gap-10 justify-between flex-wrap">
+        <div className="w-[370px] h-[465px] shadow-lg">
+                  <div className="h-[370px] overflow-hidden relative group">
+                    <Image className="h-full w-full object-cover" source={productImage}/>
+                    {
+                      newProduct &&
+                        <div className="absolute top-5 left-5">
+                          <span className='py-[10px] px-8 text-[#FFFFFF] text-sm font-bold font-dm bg-[#262626]'>New</span>
+                        </div>
+                    }
+                    <div className="w-full h-[156px] bg-white absolute bottom-0 translate-y-full group-hover:translate-y-0 transition-all duration-500 p-6">
+                      <ul className='flex flex-col gap-5'>
+                        <li className="flex gap-4 items-center justify-end cursor-pointer">
+                          <Link className='text-base text-[#767676] font-dm hover:text-[#000000] transition duration-300'>Add to Wish List</Link>
+                          <FaHeart className='text-sm' />
+                        </li>
+                        <li className="flex gap-4 items-center justify-end cursor-pointer">
+                          <Link className='text-base text-[#767676] font-dm hover:text-[#000000] transition duration-300'>Compare</Link>
+                          <FiRefreshCw className='text-sm' />
+                        </li>
+                        <li className="flex gap-4 items-center justify-end cursor-pointer">
+                          <Link className='text-base text-[#767676] font-dm hover:text-[#000000] transition duration-300'>Add to Cart</Link>
+                          <FaShoppingCart className='text-sm' />
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                  <div className="">
+                    <Flex className="justify-between items-center my-4">
+                    <h5 className='text-[#262626] font-bold font-dm text-xl'>{productName}</h5>
+                    <span className='text-[#767676] font-normal font-dm text-base leading-[30px]'>{productPrice}</span>
+                    </Flex>
+                    <div className="">
+                      <span className='text-[#767676] font-normal font-dm text-base leading-[30px]'>Black</span>
+                    </div>
+                  </div>
+              </div>
+    </Flex>
+  )
+}
+
+export default ProductCard
